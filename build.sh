6e2e28c9d3e38f98428587d46e9fd908ad6c8ace
PoshPoro <poshestporo2@pm.me>
2026-08-11 20:51:48 +0100

    remove 32 bit build variants

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# NOTE: This includes 32bit installation flags if we ever need them
+
 # May be able to minimise these flags
-FFMPEG_FLAGS="--disable-static --enable-shared --enable-gpl --enable-version3 --disable-w32threads --enable-bzlib --enable-fontconfig --enable-gnutls --enable-iconv --enable-libfreetype --enable-libgsm --enable-librtmp --enable-libtheora --enable-libtwolame --enable-libvidstab --enable-libvpx --enable-libx264 --enable-libxvid --enable-zlib"
+FFMPEG_FLAGS+="--disable-static --enable-shared --enable-gpl --enable-version3 --disable-w32threads --enable-bzlib --enable-fontconfig --enable-gnutls --enable-iconv --enable-libfreetype --enable-libgsm --enable-librtmp --enable-libtheora --enable-libtwolame --enable-libvidstab --enable-libvpx --enable-libx264 --enable-libxvid --enable-zlib"
 
 # Change these to update ffmpeg
 BRANCH="release/8.1"
@@ -36,25 +38,6 @@ generic_build() {
     make install
 }
 
-linux_build() {
-    if [ "$(uname -m)"  == "x86" ]; then
-        FFMPEG_FLAGS+='--extra-cflags="-m32" --extra-ldflags="-m32"'
-    fi
-    generic_build
-}
-
-windows_build() {
-    if [ $1 == "x86" ]; then
-        FFMPEG_FLAGS+='--arch=x86 --target-os=mingw32 --cross-prefix=i686-w64-mingw32'
-    elif [ $1 == "x86_64" ]; then
-        FFMPEG_FLAGS+='--arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32'
-    else
-        echo "Invalid parameter to windows_build, only x86 and x86_64 are accepted - you put in $1"
-        exit
-    fi
-    generic_build
-}
-
 make_workspace
 cd workspace
-linux_build
+generic_build
